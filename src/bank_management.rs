fa9d9309e0c11faeacaf5a90ab0a34e85257f5f2
Ronnie-Ahmed <rksraisul@gmail.com>
2024-04-12 15:47:35 +0600

    Bank Management System

--- src/bank_management.rs
+++ src/bank_management.rs
@@ -1,6 +1,5 @@
-use core::num;
 use std::collections::HashMap;
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
 
 pub struct Account{
     account_id:u32,
@@ -13,8 +12,8 @@ impl Account{
         Account { account_id: account_id, account_name: account_name.to_string(), balance: 0.0 }
     }
 
-    pub fn get_account(self)->String{
-        format!("Account Id: {} \n, Account name : {} \n, Balance: {}" ,self.account_id,self.account_name,self.balance)
+    pub fn get_account(&self)->String{
+        format!("Account Id: {} , Account name : {} , Balance: {}" ,self.account_id,self.account_name,self.balance)
     }
 
     pub fn deposit(&mut self,amount:f32){
@@ -81,14 +80,16 @@ pub fn bank_management(){
                 accounts.insert(account_id, Account::new(account_id, &name));
                 println!("Account Created Successfully");
                 println!("......................................................................................");
+                let account=accounts.get(&account_id).unwrap();
+                println!("{:?}",account.get_account());
                 account_id+=1;
             },
             2=>{
-                let mut account_number=get_account_number();
+                let  account_number=get_account_number();
                 let account=accounts.get_mut(&account_number);
                 match account{
                     Some(acc)=>{
-                        let mut amount=get_amount("Enter the amount you want to deposit");
+                        let  amount=get_amount("Enter the amount you want to deposit");
                         acc.deposit(amount);
                         println!("Amount Deposited Successfully");
                     }
@@ -97,8 +98,50 @@ pub fn bank_management(){
                     }
                 }
 
+            },
+            3=>{
+                let  account_number=get_account_number();
+                let account=accounts.get_mut(&account_number);
+                match account{
+                    Some(acc)=>{
+                        let  amount=get_amount("Enter the amount you want to withdraw");
+                        match acc.withdraw(amount){
+                            Ok(())=>{
+                                println!("Amount Withdrawn Successfully");
+                            }
+                            Err(_)=>{
+                                println!("Insufficient Amount")
+                            }
+                        }
+                       
+                    }
+                    None=>{
+                        println!("No Account Found");
+                    }
+                }
+
+            },
+            4=>{
+                let  account_number=get_account_number();
+                let account=accounts.get(&account_number).unwrap();
+                println!("Balance is : {:?}",account.get_balance());
+
             }
-            _=>println!("Redo")
+            5=>{
+                let  account_number=get_account_number();
+                let account=accounts.get(&account_number).unwrap();
+                println!("{:?}",account.get_account())
+
+
+            }
+            6=>{
+                println!("Thank you for using Bank Management System");
+                break;
+            }
+            _=>{
+                println!("Invalid Choices");
+                continue;
+            }
         }
 
 
